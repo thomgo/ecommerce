bb1493610a2ff22e72ee3829699582101cfe6676
Thomas <tg.59290@gmail.com>
2020-02-14 16:48:55 +0100

    correct db connexion, wrong db name

--- version_bdd/Model/db.php
+++ version_bdd/Model/db.php
@@ -2,9 +2,9 @@
 //On essaie de se connecter à la base de données avec nos identifiants
 //Si cela marche pon crée une variable db qui stocke la connexion
 try {
-  $db = new PDO("mysql:host=localhost;dbname=ecommerce", "root", "ThomAdmin12");
+  $db = new PDO("mysql:host=localhost;dbname=commerce", "root", "ThomAdmin12");
 }
-//Sinon on récupère une erreur 
+//Sinon on récupère une erreur
 catch (Exception $e) {
   echo 'Exception reçue : ' .  $e->getMessage() . "\n";
 }
