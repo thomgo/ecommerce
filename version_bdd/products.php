cc54f3db3be5ec68d9c87b3f45f24513f8978215
Thomas <tg.59290@gmail.com>
2020-03-02 12:21:25 +0100

    update card display on products page

--- version_bdd/products.php
+++ version_bdd/products.php
@@ -17,37 +17,40 @@ displayMessages();
  ?>
 
  <div class="row mt-5">
-    <section class="col-lg-9">
-      <h2>Nos derniers produits</h2>
-      <div class="container-fluide">
-        <div class="row">
-          <?php
-            //On boucle pour afficher tous les produits contenus dans la variable products
-            foreach ($products as $key => $product) {
+   <section class="col-lg-9">
+     <h2>Nos derniers produits</h2>
+     <div class="container-fluide">
+       <div class="row">
+         <?php
+           //On boucle pour afficher tous les produits contenus dans la variable products
+           foreach ($products as $key => $product) {
+         ?>
+         <article class="col-lg-6 my-4">
+           <div class="card">
+             <header class="card-header text-center darkBg">
+               <h5 class="card-title"><?php echo $product["name"] ?></h5>
+             </header>
+             <div class="card-body">
+               <!-- Permet de ne pas afficher tout le texte de description mais seulement les 150 premiers caractères -->
+               <p class="card-text"><?php echo substr($product["description"],0,150) . "..." ?></p>
+             </div>
+             <ul class="list-group list-group-flush">
+               <li class="list-group-item bg-light">Prix : <?php echo $product["price"] ?> €</li>
+               <li class="list-group-item bg-light">Lieu de production: <?php echo $product["made_in"] ?></li>
+               <li class="list-group-item bg-light">Catégorie : <?php echo $product["category"] ?></li>
+               <li class="list-group-item bg-light text-center">
+                 <a href="<?php echo 'single.php?id=' . $product['id']; ?>" class="btn lightBg">Voir</a>
+               </li>
+             </ul>
+           </div>
+         </article>
+         <?php
+         //On ferme la boucle
+           }
           ?>
-          <article class="col-lg-6 my-4">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title"><?php echo $product["name"] ?></h5>
-                <p class="card-text"><?php echo $product["description"] ?></p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Prix : <?php echo $product["price"] ?></li>
-                <li class="list-group-item">Lieu de production: <?php echo $product["made_in"] ?></li>
-                <li class="list-group-item">Catégorie : <?php echo $product["category"] ?></li>
-              </ul>
-              <div class="card-body">
-                <a href="<?php echo 'single.php?id=' . $product['id']; ?>" class="btn lightBg">Voir</a>
-              </div>
-            </div>
-          </article>
-          <?php
-          //On ferme la boucle
-            }
-           ?>
-        </div>
-      </div>
-    </section>
+       </div>
+     </div>
+   </section>
     <!-- Aside avec les informations utilisateur -->
     <?php include "Template/aside.php"; ?>
   </div>
